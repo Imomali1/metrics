b294b46b83150e324af94a9eac0c3fb8d538e830
Imomali <bwg.it2847@gmail.com>
2024-07-19 09:35:32 +0300

    added coverage action, added agent and server info

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	app "github.com/Imomali1/metrics/internal/app/agent"
+	"fmt"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func printAgentInfo() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
+
 func main() {
+	printAgentInfo()
+
 	cfg := app.LoadConfig()
 
 	app.Run(cfg)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,9 +5,24 @@ import (
 
 	app "github.com/Imomali1/metrics/internal/app/server"
 	"github.com/Imomali1/metrics/internal/pkg/logger"
+	"fmt"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func printServerInfo() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+}
+
 func main() {
+	printServerInfo()
+
 	cfg := app.LoadConfig()
 
 	log := logger.NewLogger(os.Stdout, cfg.LogLevel, cfg.ServiceName)
